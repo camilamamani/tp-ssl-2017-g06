eb085bd77d20e2fbbe1a89555bdfec6c138c8a0c
camilamamani <camilamamani38@gmail.com>
2017-04-16 21:59:09 -0300

    Update main.c

--- 051-06-01-Conversor/main.c
+++ 051-06-01-Conversor/main.c
@@ -1,12 +1,17 @@
+ /* main.c
+  * Conversor de Temperaturas
+  * Equipo 06
+  * 16/04/2017
+  */
 #include <stdio.h>
 #include "conversor.h"
 
-const int LOWER =	0; /* lower limit of table */
-const int UPPER =	300; /* upper limit */
-const int STEP 	=	20;
-
 int main(void){
+	const int LOWER =	0; /* lower limit of table */
+	const int UPPER =	300; /* upper limit */
+	const int STEP 	=	20;
 	int fahr;
+	
 	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
 		printf("%3d %6.1f\n", fahr, fahrToCels(fahr));
 	return 0;
