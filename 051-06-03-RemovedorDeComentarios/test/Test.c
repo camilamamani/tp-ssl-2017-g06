c20b6f05edd1a1beab48eb73a9a149e2e082f10c
agustiin <agu_90@hotmail.com>
2017-06-04 22:28:52 -0300

    Create Test.c

---
+++ 051-06-03-RemovedorDeComentarios/test/Test.c
@@ -0,0 +1,11 @@
+/*Para test funcional de removedor de comentarios
+ *Equipo 06
+ *20170603
+ */
+#include <stdio.h>/*gjhg*hjg*/
+
+main/*dfg*gd*/(void){
+	//hjukjhjkhkhjkh
+ 	putchar('sdadsaa\'scds');
+	printf("Hello,/*fgd*/ \" World!");//565695695ds/
+}
