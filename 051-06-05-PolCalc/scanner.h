365d072ad0064ce4cd2dc46d31b16f6d8743bb83
Camila Mamani <camilamamani38@gmail.com>
2017-07-02 23:58:08 -0300

    Add files via upload

---
+++ 051-06-05-PolCalc/scanner.h
@@ -0,0 +1,35 @@
+ /* scanner.h
+  * Equipo 06
+  * 02/07/2017
+  */
+#ifndef SCANNER_H
+#define SCANNER_H
+
+/*
+Qué exporta:
+	▪ GetNextToken
+	▪ Token
+	▪ TokenType
+	▪ TokenValue
+*/
+enum TokenType {
+ Number,
+ Addition='+',
+ Multiplication='*',
+ Substraction='-',
+ Division='/',
+ PopResult='\n',
+ LexError
+};
+
+typedef enum TokenType TokenType;
+typedef double TokenValue;
+
+struct Token{
+ TokenType type;
+ TokenValue val;
+};
+
+bool GetNextToken(Token *t);
+
+#endif
