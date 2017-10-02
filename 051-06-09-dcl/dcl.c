797182098ff05381e928e7e5b7f44fac4cbefc4e
Camila Mamani <camilamamani38@gmail.com>
2017-10-01 22:57:08 -0300

    Add files via upload

---
+++ 051-06-09-dcl/dcl.c
@@ -0,0 +1,121 @@
+ /* dcl.c
+  * Equipo 06
+  * 02/09/2017
+  */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+
+enum {NAME, PARENS, BRACKETS};
+
+void dcl();
+void dirdcl();
+
+int gettoken();
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[1000];
+
+int main(){
+	while(gettoken() != EOF){
+		strcpy(datatype, token);
+		out[0] = '\0';
+		dcl();
+
+		if(tokentype != '\n')
+			printf("error de sintaxis\n");
+		
+		printf("%s: %s %s\n", name, out, datatype);
+	}
+	return 0;
+}
+
+int gettoken(){
+	int c;
+	char *p = token;
+
+	while((c = getchar()) == ' ' || c == '\t')
+		;
+
+	if(c == '('){
+		if((c = getchar()) == ')'){
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		}else{
+			ungetc(c, stdin);
+			return tokentype ='(';
+		}
+	}else if (c=='['){
+		for(*p++ = c;(*p++ = getchar()) != ']';)
+			;
+		
+		*p='\0';
+		return tokentype = BRACKETS; 
+
+	} else if (isalpha(c)){
+		for(*p++ = c; isalnum(c = getchar());)
+			*p++ = c;
+		
+		*p='\0';
+		ungetc(c, stdin);
+		return tokentype = NAME;
+	} else
+		return tokentype = c;
+}	
+
+/* dcl: reconoce una declaración */
+void dcl(){
+	int ns;
+	for(ns = 0; gettoken() == '*';)
+		ns++;
+	dirdcl();
+	while(ns-- > 0)
+		strcat(out, "apuntador a ");
+}
+
+/* dirdcl: reconoce un declarador directo */
+void dirdcl(){
+	int type;
+
+	if(tokentype == '('){
+		dcl();
+		if(tokentype != ')')
+			printf("error: falta )\n");
+	}else if(tokentype == NAME)
+		strcpy(name, token);
+	else
+		printf("error: nombre o (dcl) esperado\n");
+
+	while((type=gettoken()) == PARENS || type == BRACKETS){
+		if(type == PARENS)
+			strcat(out, "funcion que regresa");
+		else{
+			strcat(out, " arreglo");
+			strcat(out, token);
+			strcat(out, "de");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 051-06-09-dcl/readme.md
@@ -0,0 +1,13 @@
+# Sintaxis y Semántica de los Lenguajes
+- Curso: k2051.
+- Año 2017.
+- Grupo 06.
+- Integrantes:
+  - Federico Hazama
+  - Agustin Pérez
+  - Camila Mamani
+  - Luciano Lisachi
+  - Leandro Lacave
+  
+ **TP N9 dcl convierte un declaración de C en una descripción hecha con palabras**
+ 
